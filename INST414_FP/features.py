656867c07a8364f7304740e53d5738c891c12304
1oduran1 <oduran1@terpmail.umd.edu>
2025-05-09 13:16:09 -0400

    features for training and holdout

--- INST414_FP/features.py
+++ INST414_FP/features.py
@@ -20,6 +20,7 @@ from tqdm import tqdm
 import typer
 import pandas as pd
 
+
 from INST414_FP.config import PROCESSED_DATA_DIR
 
 app = typer.Typer()
@@ -28,23 +29,25 @@ app = typer.Typer()
 @app.command()
 def main(
     # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = PROCESSED_DATA_DIR / "Clean_Final_Full_Housing_Classification.csv",
+    input_path: Path = PROCESSED_DATA_DIR,
     output_path: Path = PROCESSED_DATA_DIR,
     # -----------------------------------------
 ):
     # ---- REPLACE THIS WITH YOUR OWN CODE ----
     logger.info("Generating features from dataset...")
     
-    full_data = pd.read_csv(input_path)
-    
-    full_data.sort_values(by=['Date'])
-    
-    features = full_data.drop(columns=['Affordability', 'Date'])
-    labels = full_data['Affordability']
-    
-    features.to_csv(output_path/ "features.csv", index=False)
-    labels.to_csv(output_path/ "labels.csv", index=False)
-    logger.success("Features generation complete.")
+    datasets = {'train': pd.read_csv(input_path/'clean_training_data.csv'),
+                'holdout': pd.read_csv(input_path/'clean_holdout_data.csv')}
+
+    for name, data in datasets:
+        data.sort_values(by=['Date'])
+        features = data.drop(columns=['Affordability', 'Date'])
+        labels = data['Affordability']
+        features.to_csv(output_path/ f"{name}_features.csv", index=False)
+        logger.info(f"Saved {name} to {output_path/ f'{name}_features.csv'}")
+        labels.to_csv(output_path/ f"{name}_labels.csv", index=False)
+        logger.info(f"Saved {name} to {output_path/ f'{name}_labels.csv'}")
+
     # -----------------------------------------
 
 
